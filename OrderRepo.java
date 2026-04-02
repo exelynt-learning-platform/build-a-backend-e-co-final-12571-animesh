01ae1befac142e3057a0ce7e6da1836276b0dd32
Animesh Choudhury <94227992+Darkdodo004@users.noreply.github.com>
2026-04-02 20:48:11 +0530

    Delete OrderRepo.java

--- OrderRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order, Long> {}
