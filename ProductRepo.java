0d5b88a7c52dab27daffd6c7ad811de346f068e5
Animesh Choudhury <94227992+Darkdodo004@users.noreply.github.com>
2026-04-02 20:49:26 +0530

    Delete ProductRepo.java

--- ProductRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {}
